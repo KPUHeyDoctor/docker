842607a9cf3ca66984cd5589ad94eeeb554d2ea7
0sik <ghdtlrdudtlr@naver.com>
2023-03-28 02:08:36 +0900

    docs : init 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains="TUKDOCTOR.shop"
 rsa_key_size=4096
 data_path="./certbot"
-email="tkdals09789@gmail.com" # Adding a valid address is strongly recommended
+email="ghdtlrdudtlr@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
